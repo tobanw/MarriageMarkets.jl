3a2ed84bb61cfcb660cdd68b25402453a8c5a58d
Toban Wiebe <tobanw@gmail.com>
2016-10-27 14:58:08 -0400

    add value function update step

--- src/shimer-smith.jl
+++ src/shimer-smith.jl
@@ -82,28 +82,30 @@ type ShimerSmith
 	### Equilibrium computation ###
 
 	"""
-		update_singles(ρ, δ, ℓ_m, ℓ_f, u_m, u_f, α)
+		update_singles!(ρ, δ, ℓ_m, ℓ_f, u_m, u_f, α)
 
 	Compute the implied singles distributions given a matching function from the
 	steady-state equilibrium conditions.
 
 	For males, this equation is:
-	``∀x, δ(ℓ(x) - u(x)) = ρ u(x) ∫α(x,y)u(y)dy``.
+	``∀x, δ(ℓ(x) - u(x)) = ρ u(x) ∫ α(x,y) u(y) dy``.
 	Thus, this function solves a non-linear system of equations for `u_m` and `u_f`.
 	"""
 	function update_singles!(ρ::Float64, δ::Float64, α::Array,
 						  ℓ_m::Vector, ℓ_f::Vector, u_m::Vector, u_f::Vector)
 
 		"""
-		Split vector into male/female, then calculate residuals and recombine.
+		Vector function representing the system of equations to be solved.
 		"""
 		function vecsys!(μ::Vector, res::Vector) # vec'd
 			# split by sex
 			μm = μ[1:length(u_m)]
 			μf = μ[length(u_m)+1:end]
 
+			# compute residuals of non-linear system
 			mres = δ * ℓ_m - μm .* (δ + ρ * (α * μf))
 			fres = δ * ℓ_f - μf .* (δ + ρ * (α' * μm))
+
 			res[:] = [vec(mres); vec(fres)] # concatenate into big vector
 		end # vecsys!
 
@@ -120,21 +122,53 @@ type ShimerSmith
 	end # update_singles!
 
 	"""
-	Calculate singlehood value functions.
+		update_values!(ρ, δ, r, α, h, u_m, u_f, w_m, w_f)
+
+	Compute the implied singlehood value functions given a matching function
+	and singles distributions.
+
+	For males, the value function equation is:
+	``∀x, w(x) = \frac{ρ}{2(r+δ)} ∫ (h(x,y) - w(x) - w(y)) α(x,y) u(y) dy``.
+	Thus, this function solves a non-linear system of equations for `w_m` and `w_f`.
 	"""
 	function update_values!(ρ::Float64, δ::Float64, r::Float64, α::Array, h::Array,
 						 u_m::Vector, u_f::Vector, w_m::Array, w_f::Array)
-		# similar system of equations as in update_singles
+		θ = ρ / (2*(r+δ)) # precompute constant
+
+		"""
+		Vector function representing the system of equations to be solved.
+		"""
+		function vecsys!(ω::Vector, res::Vector) # vec'd
+			# split by sex
+			ωm = ω[1:length(w_m)]
+			ωf = ω[length(w_m)+1:end]
+
+			αS = α .* match_surplus(h, ωm, ωf)
+
+			# compute residuals of non-linear system
+			mres = ωm - θ * (αS * u_f)
+			fres = ωf - θ * (αS' * u_m)
+
+			res[:] = [vec(mres); vec(fres)] # concatenate into big vector
+		end # vecsys!
+
+		# initial guess of value functions: stacked vector of previous values
+		guess = [w_m; w_f]
+
+		# NLsolve
+		result = nlsolve(vecsys!, guess, ftol=1e-16)
+
+		w_m[:] = result.zero[1:length(w_m)]
+		w_f[:] = result.zero[length(w_m)+1:end]
+
+		return w_m, w_f
 	end
 
 	"""
-	Calculate matching function α from S ≧ 0 condition.
+	Calculate matching function α from S ≥ 0 condition.
 	"""
 	function update_match!(h::Array, w_m::Array, w_f::Array, α::Array)
-		for i in 1:length(w_m), j in 1:length(w_f)
-			α[i,j] = 1*(h[i,j] - w_m[i] - w_f[j] ≥ 0)
-		end
-
+		α[:,:] = 1*(match_surplus(h, w_m, w_f) .≥ 0.0)
 		return α
 	end
 
@@ -147,16 +181,27 @@ type ShimerSmith
 	Construct production array from function.
 	"""
 	function prod_array(mtypes::Vector, ftypes::Vector, prodfn::Function)
-		# generate surp from prodfn and types
-		surp = Array{Float64}(length(mtypes), length(ftypes))
+		h = Array{Float64}(length(mtypes), length(ftypes))
 
 		for (i,x) in enumerate(mtypes), (j,y) in enumerate(ftypes)
-			surp[i,j] = prodfn(x, y)
+			h[i,j] = prodfn(x, y)
 		end
 
-		return surp
+		return h
 
 	end # prod_array
 
-end # type
+	"""
+	Construct match surplus array from value functions.
+	"""
+	function match_surplus(h::Array, w_m::Vector, w_f::Vector)
+		S = similar(h)
 
+		for i in 1:length(w_m), j in 1:length(w_f)
+			S[i,j] = h[i,j] - w_m[i] - w_f[j]
+		end
+
+		return S
+	end
+
+end # type
